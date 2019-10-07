55783e1440ddadc1e5eb76599495cc7d7b7da29f
100148965 <canterojessie@gmail.com>
2019-10-07 15:43:36 -0500

    did some work with strings and printing

---
+++ ojo.py
@@ -0,0 +1,42 @@
+# more strings and text
+
+x = "there are %d types of people." % 10
+binary = "binary"
+doNot = "don't"
+y = "those who know %s and those who know %s" % (binary, doNot)
+
+print(x)
+print(y)
+
+print("I said: %r.:" % x)
+print("i also said: '%s'." % y)
+
+hilarious = True
+jokeEvaluation = "isn't that joke so funny!? %r"
+
+print(jokeEvaluation % hilarious)
+
+w = "this is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+# more printing fun
+print("mary had a little lamb.")
+print("it's fleece was white as %s." % 'snow')
+print("and everywhere that mary went.")
+print("." * 10)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6,)
+print(end7 + end8 + end9 + end10 + end11 + end12)
