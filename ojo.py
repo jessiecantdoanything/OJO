7d38a04887ff70cfb1d59d5ec1fb24fa06952ff4
100148965 <canterojessie@gmail.com>
2019-10-09 15:16:19 -0500

    typing stuff he put last minute but finishing it two days later because i never ended up finishing the code

--- ojo.py
+++ ojo.py
@@ -42,9 +42,9 @@ end12 = "r"
 print(end1 + end2 + end3 + end4 + end5 + end6,)
 print(end7 + end8 + end9 + end10 + end11 + end12)
 
-# but wait! there's more.
+# More Formatting
 formatter = "%r %r %r %r"
-print(formatter %)
-print(formatter %)
-print(formatter %)
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
 print(formatter % (formatter, formatter, formatter, formatter))
